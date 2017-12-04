e596faed7101b83fb05536c09a3ffc7dc6cec706
valdirivaaal <rivaldi.alamsyah97@gmail.com>
2017-12-04 10:15:55 +0700

    update travis yml lagi #15

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -29,10 +29,10 @@ abstract class DuskTestCase extends BaseTestCase
      */
     protected function driver()
     {
-        $options = (new ChromeOptions)->addArguments([
-            '--disable-gpu',
-            '--headless'
-        ]);
+        // $options = (new ChromeOptions)->addArguments([
+        //     '--disable-gpu',
+        //     '--headless'
+        // ]);
 
         return RemoteWebDriver::create(
             'http://localhost:9515', DesiredCapabilities::chrome()
